c49f68a2e498f6bfb3a1e42f93bf06edbe2a0d59
Tom <tomtarpeydev@gmail.com>
2019-10-01 11:11:14 +0100

    challenge solution 1

--- src/challenge.py
+++ src/challenge.py
@@ -6,10 +6,31 @@
 # you may assume that the list is length 3 or more
 
 # you can make your own sort or you can use a sort function built in
+def centered_average(ints):
+    # get the smallest and largest numbers
+    smallest = min(ints)
+    largest = max(ints)
 
+    # set an initial value of the sum
+    sum = 0
+    # itterate over the ints
+    for i in ints:
 
-# centered_average([1, 2, 3, 4, 100]) → 3
-# centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
-# centered_average([-10, -4, -2, -4, -2, 0]) → -3
+        # sum up the ints
+        sum += i
+    
+    # set the sum to the sum - smallest - largest
+    sum = sum - smallest - largest
+    # return sum / (len(ints) - 2)
+    return int(sum / (len(ints) - 2))
+
+a = centered_average([1, 2, 3, 4, 100]) # → 3
+b = centered_average([1, 1, 5, 5, 10, 8, 7]) # → 5
+c = centered_average([-10, -4, -2, -4, -2, 0]) # → -3
+
+print(a)
+print(b)
+print(c)
+
 
 
