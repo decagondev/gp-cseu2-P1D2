e9fed2cb78f5bbc7b9cfdbf2ad1a153cd4feed61
Tom <tomtarpeydev@gmail.com>
2019-10-01 08:46:53 +0100

    (init) - added functions.py

---
+++ src/functions.py
@@ -0,0 +1,13 @@
+# how to define a function
+
+# define a doubling function that passes args by value
+
+
+# define a doubling function that passes args by reference
+
+
+    # no return necessary -
+    # passing by reference means that
+    # original list is modified
+
+# try out our functions
