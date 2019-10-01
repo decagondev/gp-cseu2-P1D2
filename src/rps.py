3fa6708e54afbe5cef6ec836ddf8f6e4e3d2b916
Tom <tomtarpeydev@gmail.com>
2019-10-01 12:28:32 +0100

    Step 1 and 2 polya (understand and plan)

--- src/rps.py
+++ src/rps.py
@@ -1,1 +1,23 @@
 # create a simple rock paper scisors game in python
+
+# Create a rock/paper/scissors REPL
+# Have a computer AI to play agains us
+# keep track of a score
+# RULES: r beats s, s beats p, p beats r
+
+# set up some sentinal values for variables
+wins = 0
+losses = 0
+ties = 0
+# set up a list of possible choices
+choices = ["r", "p", "s"]
+# start a REPL
+
+    # show score
+    # take input from the player
+
+    # have the AI make a random choice of r/p/s
+
+    # show the AI choice
+
+    # conditional logic or commande
