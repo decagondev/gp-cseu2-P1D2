eefb7ff8f442964ff8ea62c92d561532778f00f9
Tom <tomtarpeydev@gmail.com>
2019-10-01 10:54:14 +0100

    challenge

--- src/challenge.py
+++ src/challenge.py
@@ -1,8 +1,15 @@
 # Return the "centered" average of a list of ints, 
 # which we'll say is the mean average of the values, 
 # except ignoring the largest and smallest values in the array. 
+# if there are multiple copies of largest or smallest values. ignore only 1 of min / max respectively
+# the input list may be unsorted
+# you may assume that the list is length 3 or more
+
+# you can make your own sort or you can use a sort function built in
+
 
 # centered_average([1, 2, 3, 4, 100]) → 3
 # centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
 # centered_average([-10, -4, -2, -4, -2, 0]) → -3
+
 
