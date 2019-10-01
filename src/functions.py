cfd02203990e885bb7ff56d8f02a95761d5fe97e
Tom <tomtarpeydev@gmail.com>
2019-10-01 10:29:07 +0100

    multiply by val

--- src/functions.py
+++ src/functions.py
@@ -2,13 +2,19 @@
 # def someFunc(x):
 #     return x
 name = "dave"
+l = [1, 2, 3, 4]
 
 def cap(n):
     return n.capitalize()
 # define a doubling function that passes args by value
-
+def mult(x):
+    return x * 2
 # define a doubling function that passes args by reference
 
+def mult2(lst):
+    for i in range(len(lst)):
+       # lst[i] = lst[i] * 2
+       lst[i] *= 2
     # no return necessary -
     # passing by reference means that
     # original list is modified
@@ -18,3 +24,7 @@ def cap(n):
 print(name) # => "dave"
 cap(name) # => "Dave" pass by val
 print(name) # => "dave"
+n = 12
+print(n)
+print(mult(n))
+print(n)
