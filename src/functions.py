b56642756620bb475abc5581a7f133300670b11e
Tom <tomtarpeydev@gmail.com>
2019-10-01 10:21:23 +0100

    cap function - pass by val

--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,13 @@
 # how to define a function
 # def someFunc(x):
 #     return x
+name = "dave"
 
+def cap(n):
+    return n.capitalize()
 # define a doubling function that passes args by value
 
-
 # define a doubling function that passes args by reference
-
 
     # no return necessary -
     # passing by reference means that
@@ -14,3 +15,6 @@
 
 # try out our functions
 # var1 = someFunc("Hello") # => "Hello"
+print(name) # => "dave"
+cap(name) # => "Dave" pass by val
+print(name) # => "dave"
