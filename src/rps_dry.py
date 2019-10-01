278b77ee03cbdb5f80450149dbed8df328ce5478
Tom <tomtarpeydev@gmail.com>
2019-10-01 13:08:21 +0100

    RPS DRY (Second Pass DRY solution)

--- src/rps_dry.py
+++ src/rps_dry.py
@@ -14,15 +14,23 @@ ties = 0
 choices = ["r", "p", "s"]
 
 # a function that compares choices
-
+def compare_choices(player_choice, computer_choice):
     #test if player_choice is equal to computer_choice
+    if player_choice == computer_choice:
         # return 0
+        return 0
     # otherwise (if pc is r and cc is s) 
     # or (pc is p and cc is r) or
     # (pc s and cc is p)
+    elif (player_choice == "r" and computer_choice == "s") or \
+         (player_choice == "p" and computer_choice == "r") or \
+         (player_choice == "s" and computer_choice == "p"):
         # return 1
+        return 1
     # otherwise
+    else:
         # return -1
+        return -1
 
 # start a REPL
 while True:
@@ -35,8 +43,20 @@ while True:
     # show the AI choice
     print(f"Computer Chose {ai_choice}")
     # conditional logic or commande
-    
-    else:
-        print("I do not understand that command")
+    # compare the choices
+    results = compare_choices(cmd, ai_choice)
+
+    if cmd == "q":
+        print("Goodbye!")
+        break
+    if results == 1:
+        wins += 1
+        print("You Win!")
+    elif results == -1:
+        losses += 1
+        print("You Lose!")
+    elif results == 0:
+        ties += 1
+        print("You Tied!")
         
         
