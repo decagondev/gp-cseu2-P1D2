420cb909e928b81c2ee3d195afa97165dff384de
Tom <tomtarpeydev@gmail.com>
2019-10-01 16:14:56 +0100

    space map (polya 1 and 2)

--- src/game/space-map.py
+++ src/game/space-map.py
@@ -1,1 +1,28 @@
 # implement class to deal with the map
+
+# space class with rows and cols
+    # constructor
+        # Allocate list to hold space
+        # rows
+
+        # iterate over the rows and add cols
+            # array of 'cols' '.'s
+            # append the row to space
+        
+        # throw some stars in about 7% of space
+        # work out 7% of map
+
+        # iterate over stars
+            # random row star
+            # random col star
+            # add the '*' to space at row and col
+
+    # print map function
+        # iterate over space rows
+            # iterate over space cols
+                # write the col
+            # print empty line
+
+# instantiate a space object
+
+# print the map
