b9ee37c6ffa014b358fdb1e22bafb9c196438ee2
Tom <tomtarpeydev@gmail.com>
2019-10-01 16:21:13 +0100

    Space Constructor

--- src/game/space-map.py
+++ src/game/space-map.py
@@ -1,21 +1,34 @@
-# implement class to deal with the map
+import sys
+import random
 
+# implement class to deal with the map
+class Space:
 # space class with rows and cols
     # constructor
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
         # Allocate list to hold space
-        # rows
+        self.space = [] # rows of space
 
         # iterate over the rows and add cols
-            # array of 'cols' '.'s
+        for i in range(rows):
+            newRow = ['.'] * cols # array of 'cols' '.'s
             # append the row to space
+            self.space.append(newRow)
         
         # throw some stars in about 7% of space
+        starCount = int(rows * cols * 0.07)
         # work out 7% of map
 
         # iterate over stars
+        for i in range(starCount):
             # random row star
+            rowNum = random.randint(0, len(self.space) - 1)
             # random col star
+            colNum = random.randint(0, len(self.space) - 1)
             # add the '*' to space at row and col
+            self.space[rowNum][colNum] = '*'
 
     # print map function
         # iterate over space rows
