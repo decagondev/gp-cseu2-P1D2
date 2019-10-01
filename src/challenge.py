01570e63090314e8a2ffed31863f3fdda8eaa460
Tom <tomtarpeydev@gmail.com>
2019-10-01 08:48:45 +0100

    (init) - added challenge.py

---
+++ src/challenge.py
@@ -0,0 +1,8 @@
+# Return the "centered" average of an array of ints, 
+# which we'll say is the mean average of the values, 
+# except ignoring the largest and smallest values in the array. 
+
+# centered_average([1, 2, 3, 4, 100]) → 3
+# centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+# centered_average([-10, -4, -2, -4, -2, 0]) → -3
+
